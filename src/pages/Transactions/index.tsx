cd9b69d61b4ff14b579cf2541f41bd73af4c5206
Douglas Scaini <douglasscaini.dev@gmail.com>
2022-08-09 20:59:11 -0300

    Table + Search Form Container

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <SearchForm />
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>09/08/2022</td>
+                        </tr>
+
+                        <tr>
+                            <td width="50%">Comida</td>
+                            <td>
+                                <PriceHighlight variant="outcome">- R$ 1.000,00</PriceHighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/08/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     );
 }
