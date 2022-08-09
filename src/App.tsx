edd611a5ed8bb18895f12e3134c66c748142911f
Douglas Scaini <douglasscaini.dev@gmail.com>
2022-08-08 21:49:52 -0300

    Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
+
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
     return (
         <ThemeProvider theme={defaultTheme}>
-            <h1>app</h1>
+            <Transactions />
 
             <GlobalStyle />
         </ThemeProvider>
